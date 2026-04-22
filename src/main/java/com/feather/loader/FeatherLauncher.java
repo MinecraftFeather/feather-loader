1c09ed935ee0397e4f057925fe2ee83cbd81d83a
dabdob-craft <dabdobcraft2@gmail.com>
2026-04-22 22:46:26 +0100

    Add FeatherLauncher class with main method

---
+++ src/main/java/com/feather/loader/FeatherLauncher.java
@@ -0,0 +1,9 @@
+package com.feather.loader;
+
+public class FeatherLauncher {
+    public static void main(String[] args) {
+        System.out.println("Feather Loader is a Java Agent based mod loader.");
+        System.out.println("To use it, add this to your JVM arguments:");
+        System.out.println("-javaagent:feather-loader.jar");
+    }
+}
