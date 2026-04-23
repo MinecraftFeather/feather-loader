59a995e1a2e1349bc164bd5f5aedec8ab28e7dd1
dabdob-craft <dabdobcraft2@gmail.com>
2026-04-23 12:09:31 +0100

    Correct class reader usage and update comments
    Fix typo in class reader acceptance and clean up comments.

---
+++ src/main/java/com/feather/loader/transformer/FeatherTransformer.java
@@ -0,0 +1,41 @@
+package com.feather.loader.transformer;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.security.ProtectionDomain;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.*;
+
+public class FeatherTransformer implements ClassFileTransformer {
+    @Override
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
+                            ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+        
+        if (className.equals("net/minecraft/client/gui/screens/TitleScreen")) {
+            System.out.println("[Feather] Modifying TitleScreen...");
+            return modifyBytecode(classfileBuffer);
+        }
+        
+        return classfileBuffer;
+    }
+
+    private byte[] modifyBytecode(byte[] bytes) {
+        ClassNode classNode = new ClassNode();
+        ClassReader classReader = new ClassReader(bytes);
+        classReader.accept(classNod, 0);
+      
+        for (MethodNode method : classNode.methods) {
+            for (AbstractInsnNode insn : method.instructions) {
+                if (insn instanceof LdcInsnNode ldc) {
+                    if (ldc.cst.equals("Minecraft")) {
+                        ldc.cst = "Minecraft (Feather)";
+                    }
+                }
+            }
+        }
+
+        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        classNode.accept(classWriter);
+        return classWriter.toByteArray();
+    }
+}
