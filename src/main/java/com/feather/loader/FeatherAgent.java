1daf5e7b949be7269818a4b573fa9ac752ef795e
dabdob-craft <dabdobcraft2@gmail.com>
2026-04-23 12:28:07 +0100

    Integrate MappingManager in FeatherAgent initialization
    Added MappingManager to load mappings during initialization.

--- src/main/java/com/feather/loader/FeatherAgent.java
+++ src/main/java/com/feather/loader/FeatherAgent.java
@@ -2,6 +2,7 @@ package com.feather.loader;
 
 import com.feather.loader.api.FeatherMod;
 import com.feather.loader.transformer.FeatherTransformer;
+import com.feather.loader.mappings.MappingManager;
 import java.io.File;
 import java.lang.instrument.Instrumentation;
 import java.util.jar.JarFile;
@@ -12,7 +13,9 @@ public class FeatherAgent {
     public static void premain(String agentArgs, Instrumentation inst) {
         System.out.println("---------------------------------------");
         System.out.println("  [Feather] Initializing Engine...     ");
-        System.out.println("  [Feather] Organization: MinecraftFeather");
+        
+        MappingManager.loadMappings();
+        System.out.println("  [Feather] Mappings Loaded");
 
         File modsFolder = new File("mods");
         if (!modsFolder.exists()) {
@@ -37,7 +40,7 @@ public class FeatherAgent {
         } catch (Exception e) {
             System.err.println("  [Feather] Failed to initialize Transformer");
         }
-
+        
         try {
             ServiceLoader<FeatherMod> loader = ServiceLoader.load(FeatherMod.class);
             for (FeatherMod mod : loader) {
