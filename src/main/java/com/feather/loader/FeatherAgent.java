382d73115efc7c535a2c6e479caa37cc27f2d40c
dabdob-craft <dabdobcraft2@gmail.com>
2026-04-25 16:40:37 +0100

    Refactor FeatherAgent to use ModLoader

--- src/main/java/com/feather/loader/FeatherAgent.java
+++ src/main/java/com/feather/loader/FeatherAgent.java
@@ -2,12 +2,8 @@ package com.feather.loader;
 
 import com.feather.loader.transformer.FeatherTransformer;
 import com.feather.loader.mappings.MappingManager;
-import java.io.File;
-import java.io.InputStreamReader;
+import com.feather.loader.core.ModLoader;
 import java.lang.instrument.Instrumentation;
-import java.util.jar.JarFile;
-import java.util.jar.JarEntry;
-import java.util.Scanner;
 
 public class FeatherAgent {
 
@@ -18,17 +14,7 @@ public class FeatherAgent {
         MappingManager.loadMappings();
         System.out.println("  [Feather] Mappings Loaded");
 
-        File modsFolder = new File("mods");
-        if (!modsFolder.exists()) {
-            modsFolder.mkdirs();
-        }
-
-        File[] files = modsFolder.listFiles((dir, name) -> name.endsWith(".jar"));
-        if (files != null) {
-            for (File file : files) {
-                loadMod(file, inst);
-            }
-        }
+        ModLoader.loadMods(inst);
 
         try {
             inst.addTransformer(new FeatherTransformer());
@@ -39,42 +25,5 @@ public class FeatherAgent {
 
         System.out.println("  [Feather] Engine Ready to fly!       ");
         System.out.println("---------------------------------------");
-    }
-
-    private static void loadMod(File file, Instrumentation inst) {
-        try {
-            JarFile jarFile = new JarFile(file);
-            inst.appendToSystemClassLoaderSearch(jarFile);
-            
-            JarEntry entry = jarFile.getJarEntry("feather.mod.json");
-            if (entry != null) {
-                Scanner s = new Scanner(jarFile.getInputStream(entry)).useDelimiter("\\A");
-                String result = s.hasNext() ? s.next() : "";
-                String mainClass = parseJsonField(result, "main");
-                String modName = parseJsonField(result, "name");
-
-                if (mainClass != null) {
-                    Class<?> clazz = Thread.currentThread().getContextClassLoader().loadClass(mainClass);
-                    Object modInstance = clazz.getDeclaredConstructor().newInstance();
-                    clazz.getMethod("init").invoke(modInstance);
-                    
-                    System.out.println("  [Feather] Mod [" + modName + "] Initialized");
-                }
-            }
-            jarFile.close();
-        } catch (Exception e) {
-            System.err.println("  [Feather] Error loading mod " + file.getName() + ": " + e.getMessage());
-        }
-    }
-    private static String parseJsonField(String json, String field) {
-        try {
-            String key = "\"" + field + "\":";
-            int start = json.indexOf(key) + key.length();
-            int firstQuote = json.indexOf("\"", start) + 1;
-            int lastQuote = json.indexOf("\"", firstQuote);
-            return json.substring(firstQuote, lastQuote);
-        } catch (Exception e) {
-            return null;
-        }
     }
 }
