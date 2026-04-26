12769c8c130501d0ee6e4b4217bed64b54a9cc87
dabdob-craft <dabdobcraft2@gmail.com>
2026-04-26 12:46:46 +0100

    Refactor drawText to use reflection for Minecraft API
    Refactored drawText method to use reflection for accessing Minecraft classes and methods.

--- src/main/java/com/feather/loader/api/RenderUtils.java
+++ src/main/java/com/feather/loader/api/RenderUtils.java
@@ -1,15 +1,27 @@
 package com.feather.loader.api;
 
-import net.minecraft.client.Minecraft;
-import com.mojang.blaze3d.matrix.MatrixStack;
+import java.lang.reflect.Method;
 
 public class RenderUtils {
-    public static void drawText(MatrixStack matrixStack, String text, int x, int y, int color) {
-        Minecraft mc = Minecraft.getInstance();
-        int width = mc.getMainWindow().getScaledWidth();
-        int height = mc.getMainWindow().getScaledHeight();
-        int textWidth = mc.fontRenderer.getStringWidth(text);
-        
-        mc.fontRenderer.drawString(matrixStack, text, width - textWidth - 2, height - 10, 0xFFFFFF);
+    public static void drawText(Object matrixStack, String text, int x, int y, int color) {
+        try {
+            Class<?> mcClass = Class.forName("net.minecraft.client.Minecraft");
+            Object mc = mcClass.getMethod("getInstance").invoke(null);
+            
+            Object fontRenderer = mcClass.getField("fontRenderer").get(mc);
+            Object window = mcClass.getMethod("getMainWindow").invoke(mc);
+            
+            int width = (int) window.getClass().getMethod("getScaledWidth").invoke(window);
+            int height = (int) window.getClass().getMethod("getScaledHeight").invoke(window);
+            
+            Method getStringWidth = fontRenderer.getClass().getMethod("getStringWidth", String.class);
+            int textWidth = (int) getStringWidth.invoke(fontRenderer, text);
+            Method drawString = fontRenderer.getClass().getMethod("drawString", 
+                Class.forName("com.mojang.blaze3d.matrix.MatrixStack"), String.class, float.class, float.class, int.class);
+            
+            drawString.invoke(fontRenderer, matrixStack, text, (float)(width - textWidth - 2), (float)(height - 10), 0xFFFFFF);
+            
+        } catch (Exception e) {
+        }
     }
 }
