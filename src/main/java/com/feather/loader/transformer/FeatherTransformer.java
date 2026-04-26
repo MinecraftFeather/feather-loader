5991100e5286129eabab51ded837296666d0d811
dabdob-craft <dabdobcraft2@gmail.com>
2026-04-26 12:39:59 +0100

    Add transformation for TitleScreen class rendering
    Implemented method to transform TitleScreen rendering.

--- src/main/java/com/feather/loader/transformer/FeatherTransformer.java
+++ src/main/java/com/feather/loader/transformer/FeatherTransformer.java
@@ -1,14 +1,45 @@
 package com.feather.loader.transformer;
 
 import org.objectweb.asm.*;
-import java.lang.instrument.ClassFileTransformer;
-import java.security.ProtectionDomain;
+import org.objectweb.asm.tree.*;
 
-public class FeatherTransformer implements ClassFileTransformer {
-
+public class FeatherTransformer implements java.lang.instrument.ClassFileTransformer {
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
-                            ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+                            java.security.ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+        if (className.equals("net/minecraft/client/gui/screen/TitleScreen") || className.equals("djz")) {
+            return transformTitleScreen(classfileBuffer);
+        }
         return classfileBuffer;
+    }
+
+    private byte[] transformTitleScreen(byte[] basicClass) {
+        ClassReader reader = new ClassReader(basicClass);
+        ClassNode node = new ClassNode();
+        reader.accept(node, 0);
+
+        for (MethodNode method : node.methods) {
+            if (method.name.equals("render") || method.name.equals("a")) {
+                InsnList insns = method.instructions;
+                for (AbstractInsnNode insn : insns) {
+                    if (insn.getOpcode() == Opcodes.RETURN) {
+                        InsnList inject = new InsnList();
+                        
+                        inject.add(new VarInsnNode(Opcodes.ALOAD, 1));
+                        inject.add(new LdcInsnNode("feather loader 1.0.0-BETA"));
+                        inject.add(new IntInsnNode(Opcodes.BIPUSH, 2));
+                        inject.add(new IntInsnNode(Opcodes.BIPUSH, 10));
+                        inject.add(new IntInsnNode(Opcodes.Iconst_m1, 0)); 
+                        inject.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "com/feather/loader/api/RenderUtils", "drawText", "(Lcom/mojang/blaze3d/matrix/MatrixStack;Ljava/lang/String;III)V", false));
+                        
+                        insns.insertBefore(insn, inject);
+                    }
+                }
+            }
+        }
+
+        ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        node.accept(writer);
+        return writer.toByteArray();
     }
 }
